2daace3cbe325b7a0f7d42a6dea8f6a8bbca400e
hiepdv <hiepdv@nal.vn>
2025-12-06 10:20:53 +0700

    feat: segment_tree

---
+++ tree/segment_tree.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+const int MAXN = 1e5;
+int seg[4 * MAXN];
+
+void build(int idx, int r, int l) {
+    if (l == r) {
+        
+    } 
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
